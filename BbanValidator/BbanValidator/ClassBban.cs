bb9f3f2401b642217b8592cf592b483309b01808
Flameri <arto.lindgren1@gmail.com>
2018-02-08 09:38:29 +0200

    Started task BbanValidator

---
+++ BbanValidator/BbanValidator/ClassBban.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BbanValidator
+{
+    class ClassBban
+    {
+        // FIelds
+        public string Nordea;
+        public string Handelsbanken;
+        public string SkandinaviskaEnskildaBanken;
+        public string DanskeBank;
+        public string TapiolaPankki;
+        public string DnBNORBankASA;
+        public string Swedbank;
+        public string Spankki;
+        public string AktiaSäästöpankitPaikallisosuuspankit;
+        public string OPPohjolaryhmä;
+        public string Ålandsbanken;
+        public string SampoPankki;
+    }
+}
