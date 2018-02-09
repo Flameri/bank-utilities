85f4cce7fcb21522fb4f5213e6f7172e09f685e9
Flameri <arto.lindgren1@gmail.com>
2018-02-09 11:14:31 +0200

    Progress BbanValidator

--- BbanValidator/BbanValidator/Program.cs
+++ BbanValidator/BbanValidator/Program.cs
@@ -7,6 +7,13 @@ namespace BbanValidator
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string machineFormat = BankUtil.IsValid("423456-781");
+            Console.WriteLine();
+            
+            
+
+            Console.ReadKey();
+
         }
     }
 }
